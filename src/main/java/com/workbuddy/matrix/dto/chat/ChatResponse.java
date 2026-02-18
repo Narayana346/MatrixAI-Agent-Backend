7000f508b1c5599a87c2d6455dffd1875fff68dd
Arjun_mac <maharanan14@gmail.com>
2026-02-18 23:46:31 +0530

    main | minor import fix.

--- src/main/java/com/workbuddy/matrix/dto/chat/ChatResponse.java
+++ src/main/java/com/workbuddy/matrix/dto/chat/ChatResponse.java
@@ -1,9 +1,11 @@
 package com.workbuddy.matrix.dto.chat;
 
+import com.workbuddy.matrix.entity.ChatEvent;
 import com.workbuddy.matrix.entity.ChatSession;
 import com.workbuddy.matrix.enums.MessageRole;
 
 import java.time.Instant;
+import java.util.List;
 
 public record ChatResponse(
         Long id,
